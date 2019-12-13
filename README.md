5ef0a510d04b6472b1f7a63c392ee61f8a018297
Matthias Cullmann <matthias.cullmann@baloise.ch>
2019-12-13 17:28:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# orchstra-maven-plugin
+
+A maven plugin to manage soffico orchestra.
+
+The design goals are
+
+- keep your POM clean: no dependencies
+- keep your workspace clean : flexibilty in layout with good defaults
+ 
+## Quick start
+
+Prerequisite
+- Maven is working. That's all. No pom.xml required.
+- add jcenter repo to your maven settings
+```
+<pluginRepository>
+    <snapshots>
+        <enabled>false</enabled>
+    </snapshots>
+    <id>bintray</id>
+    <name>bintray</name>
+    <url>http://jcenter.bintray.com</url>
+</pluginRepository>
+```
+
+... to be cmpleted
+
+## Make your life easier with plugin groups
+
+Add the following to your *~/.m2/settings.xml*
+
+```
+<pluginGroups>
+    <pluginGroup>com.baloise.maven</pluginGroup>
+</pluginGroups>
+```
+
+now you can use somthing like
+
+`> mvn orchestra:deploy`
+
+[(tell me more)](http://maven.apache.org/guides/introduction/introduction-to-plugin-prefix-mapping.html#Configuring_Maven_to_Search_for_Plugins)
+
+## Configuration options
+
+Of course you have all the options as where to set the properties
+[(tell me more)](http://docs.codehaus.org/display/MAVENUSER/MavenPropertiesGuide)
+
+... to be cmpleted
+
+[![Build Status](https://travis-ci.org/baloise/jenkins-maven-plugin.svg)](https://travis-ci.org/baloise/orchstra-maven-plugin)
---
+++ src/main/java/com/baloise/maven/orchestra/OrchestraClient.java
@@ -0,0 +1,144 @@
+package com.baloise.maven.orchestra;
+
+import static java.lang.String.format;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.codehaus.plexus.util.IOUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+
+public class OrchestraClient {
+
+	public final String url;
+	private final String auth;
+	private String token;
+	private String nodeID;
+	private String tokenXML;
+
+	public OrchestraClient(URL url, String usr, String pwd) {
+		this.url = url +"/DeploymentService/Service";
+		auth = "Basic " + Base64.getEncoder().encodeToString((usr+":"+pwd).getBytes());
+	}
+
+	String post(String url, String auth, Entity entity) throws IOException {
+		HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+		con.setRequestProperty("Authorization", auth);
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "text/xml;charset=UTF-8");
+		con.setDoOutput(true);
+		try(OutputStream out = con.getOutputStream()) {			
+			entity.writeTo(out);
+		}
+		int code = con.getResponseCode();
+		String resp;
+		try(InputStream out = con.getInputStream()) {			
+			resp = IOUtil.toString(con.getInputStream());
+		}
+		if(code >=300) 
+			throw new IOException(format("response code %s : " + resp, code));
+		return resp;
+	}
+
+	void aquireDeploymentToken() throws IOException {
+		Element result = orchestraSoap("aquireDeploymentToken");
+		token = result.getElementsByTagName("emds:tokenData").item(0).getTextContent();
+		nodeID = result.getElementsByTagName("emds:nodeID").item(0).getTextContent();
+		tokenXML = "<emds:token>" + "<emds:tokenData>" + token + "</emds:tokenData>" + "<emds:nodeID>" + nodeID
+				+ "</emds:nodeID>" + "</emds:token>";
+		System.out.println(token);
+	}
+
+	void freeDeploymentToken() throws IOException {
+		orchestraSoap("freeDeploymentToken", tokenXML);
+		System.out.println(token + " freed");
+	}
+	
+	void getDeploymentInfo() throws IOException {
+		orchestraSoap("getDeploymentInfo", tokenXML);
+	}
+
+	Element orchestraSoap(String action) throws IOException {
+		return orchestraSoap(action, (out) -> {});
+	}
+
+	Element orchestraSoap(String action, String entity) throws IOException {
+		return orchestraSoap(action, (out) -> out.write((entity).getBytes()));
+	}
+	
+	public void deployScenario(Path psc) throws IOException {
+		try {
+			aquireDeploymentToken();
+			deployScenarioCallback(psc);
+			getDeploymentInfo();
+		} finally {
+			freeDeploymentToken();
+		}
+	}
+	Element deployScenarioCallback(Path psc) throws IOException {
+		return orchestraSoap("deployScenarioCallback", (out)->  {
+			out.write(( 
+					"<emds:deployScenarioCallback.Request>" + 
+					tokenXML + 
+					"<emds:serializedScenario>").getBytes());
+			
+			Files.copy(psc, Base64.getEncoder().wrap(out));
+	
+			out.write(( 
+					"</emds:serializedScenario>" + 
+					"<emds:comment>jenkins was here</emds:comment>" + 
+					"<emds:groups>" + 
+					"</emds:groups>").getBytes());
+		});
+	}
+	
+	Element orchestraSoap(String action, Entity entity) throws IOException {
+		Entity entityWrapper = entity.wrap((out) -> {
+								out.write(("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:emds=\"emds:epi:decl:server:deployment:DeploymentService\">" + 
+										"<soapenv:Header/>" + 
+										"<soapenv:Body>" + 
+										"	<emds:"+action+".Request>").getBytes());
+							
+								},
+					(out) -> {
+						out.write(("	</emds:"+action+".Request>" + 
+								"</soapenv:Body>" + 
+								"</soapenv:Envelope>").getBytes());
+					}
+		);
+		
+	   return parse(post(url, auth,entityWrapper));
+
+	}
+
+	Element parse(String resp) throws IOException {
+		System.out.println(resp);
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(new InputSource( new StringReader( resp ) ));
+					
+			doc.getDocumentElement().normalize();
+			
+			return (Element) doc.getDocumentElement()
+				.getElementsByTagName("SOAP-ENV:Body").item(0)
+				.getChildNodes().item(0);
+			
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+	}
+
+}
